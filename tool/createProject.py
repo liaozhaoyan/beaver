4d54faecd529d37aac903e4ca8efb66b64701e84
liaozhaoyan <zhaoyan.lzy@alibaba-inc.com>
2024-02-01 10:42:23 +0800

    add project create tool.

---
+++ tool/createProject.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python2
+import os
+import sys
+
+
+confBase = """
+worker:
+  - number: 1   # worker process
+    funcs:
+      - func: "httpServer"
+        mode: "TCP"
+        bind: "0.0.0.0"
+        port: 2000
+        entry: "hello"  # 
+"""
+
+
+def createProject(projName):
+    confPath = os.path.join(projName, "main")
+    os.popen("mkdir -p %s" % confPath)
+    confFile = os.path.join(confPath, "config.yaml")
+    with open(confFile, "w") as f:
+        f.write(confBase)
+    os.popen("mkdir -p %s" % os.path.join(projName, "lua/app"))
+    print("project %s is created." % projName)
+
+
+if __name__ == '__main__':
+    assert len(sys.argv) >= 3, "need path and project name args"
+    assert os.path.isdir(sys.argv[1]), "%s is not a dir" % sys.argv[1]
+    projName = os.path.join(sys.argv[1], sys.argv[2])
+    assert not os.path.exists(projName), "%s is already exist." % projName
+    createProject(projName)
