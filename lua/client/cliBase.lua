0e7dcc9352c52bc9d55428d37c042a10a91ba96c
liaozhaoyan <zhaoyan.lzy@alibaba-inc.com>
2024-04-04 09:29:00 +0800

    add sample.yaml

---
+++ lua/client/cliBase.lua
@@ -0,0 +1,75 @@
+require("eclass")
+local asyncClient = require("async.asyncClient")
+local sockComm = require("common.sockComm")
+local cffi = require("beavercffi")
+local c_type, c_api = cffi.type, cffi.api
+
+local cliBase = class("cliBase", asyncClient)
+
+function cliBase:_init_(beaver, tPort, tmo)
+    tmo = tmo or 10
+
+    asyncClient._init_(self, beaver, nil, tPort, tmo)
+    -- asyncClient will yield for connected
+end
+
+function cliBase:echo(s)
+    local res, msg = self:_waitData(s)
+    assert(res, msg)
+    if type(res) ~= "string" then
+        return nil
+    end
+    return res
+end
+
+function cliBase:_setup(fd, tmo)
+    local beaver = self._beaver
+    local co = self._coWake  -- set in asyncClient
+    local status, res
+    local e
+
+    status, e = self:cliConnect(fd, tmo)
+
+    while status == 1 do
+        if not e then
+            e = coroutine.yield()
+        end
+        local t = type(e)
+        if t == "string" then -- has data to send
+            beaver:co_set_tmo(fd, tmo)
+            res = beaver:write(fd, e)
+            if not res then
+                break
+            end
+            e = nil
+            beaver:co_set_tmo(fd, -1)
+        elseif t == "nil" then  -- host closed
+            self:wake(co, nil)
+            break
+        else  -- read event.
+            if e.ev_close > 0 then
+                break
+            elseif e.ev_in > 0 then
+                local s = beaver:read(fd, 256)
+                e = self:wake(co, s)
+                t = type(e)
+                if t == "cdata" then -->upstream need to close.
+                    assert(e.ev_close > 0)
+                    self:wake(co, nil)  -->let upstream to do next working.
+                    break
+                elseif t == "number" then  -->upstream reuse connect
+                    e = nil
+                end
+            else
+                print("IO Error.")
+                break
+            end
+        end
+    end
+
+    self._status = 0  -- closed
+    self:stop()
+    c_api.b_close(fd)
+end
+
+return cliBase
